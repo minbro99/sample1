86566570e3ff680d70ed904f82afb69072ab1fcc
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-04-12 17:26:12 +0900

    first sample
    처음 올리는 코드파일 입니다.

---
+++ operations.js
@@ -0,0 +1,17 @@
+// Math operations
+
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4); // 10 / 4 ==> 2 *  4 ==> 나머지:2
+console.log(10 % 3); // 예상값:1
+
+console.log((10 + 3 - 5) * 10);
+
+console.log('Kim' + ' ' + 'Min');
+console.log('Kim' - 'in');
+
+let userName = 'Kim';
+console.log(userName.length);
+console.log(userName.toUpperCase());
